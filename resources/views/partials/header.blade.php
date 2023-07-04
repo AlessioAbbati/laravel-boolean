09ce76012e6bae61f669e4743ac64a5726910910
Domenic0Ferrari <domenico99ferrari@gmail.com>
2023-07-04 11:45:47 +0200

    add style in homepage

---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,5 @@
+<header class="bg-primary text-light p-2 mb-4 d-flex justify-content-center gap-3">
+    <a href="#" class="btn btn-light">HOME</a>
+    <a href="#" class="btn btn-light">ABOUT US</a>
+    <a href="#" class="btn btn-light">TRAVEL</a>
+</header>
