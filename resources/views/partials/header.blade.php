deae6db63e310f698c69cdc19c51b47059f3fa27
AlessioAbbati <alex_117@live.it>
2023-07-04 13:30:17 +0200

    add index

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,5 +1,5 @@
 <header class="bg-primary text-light p-2 mb-4 d-flex justify-content-center gap-3">
     <a href="{{ route('home') }}" class="btn btn-light">HOME</a>
     <a href="{{ route('about') }}" class="btn btn-light">ABOUT US</a>
-    <a href="#" class="btn btn-light">TRAVEL</a>
+    <a href="{{ route('travels.index') }}" class="btn btn-light">YOUR TRAVELS</a>
 </header>
---
+++ resources/views/travels/index.blade.php
@@ -0,0 +1,38 @@
+@extends('layouts.base')
+
+@section('contents')
+
+    
+
+<div class="container">
+    <div class="row row-cols-3">
+        @foreach ($travels as $travel)
+            <div class="col mb-3">
+                <div class="card h-100">
+                    <img src="{{ $travel->image }}" class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <h5 class="card-title">{{ $travel->title }}</h5>
+                    </div>
+                    <ul class="list-group list-group-flush">
+                        <li class="list-group-item">{{ $travel->country }}</li>
+                        <li class="list-group-item">{{ $travel->city }}</li>
+                        <li class="list-group-item">{{ $travel->text }}</li>
+                        <li class="list-group-item">{{ $travel->date }}</li>
+                    </ul>
+                    <div class="card-body">
+                        <a href="{{ route('travels.show', ['travel' => $travel->id]) }}" class="btn btn-primary">Travel info</a>
+                        <a href="{{ route('travels.edit', ['travel' => $travel->id]) }}" class="btn btn-warning">Edit travel</a>
+                        <form action="{{ route('travels.destroy', ['travel' => $travel->id]) }}" method="POST" class="d-inline-block">
+                            @csrf
+                            @method('delete')
+                            <button class="btn btn-danger" href="">Delete</button>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        @endforeach
+    </div>
+    {{ $travels->links() }}
+</div>
+
+@endsection
