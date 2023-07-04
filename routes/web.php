fc538bd64fae9b7a143546f71c4a0f4ce10c3fe7
Domenic0Ferrari <domenico99ferrari@gmail.com>
2023-07-04 11:55:42 +0200

    add route about

--- routes/web.php
+++ routes/web.php
@@ -4,3 +4,4 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Guest\PageController;
 
 Route::get('/', [PageController::class, 'home'])->name('home');
+Route::get('/about', [PageController::class, 'about'])->name('about');
