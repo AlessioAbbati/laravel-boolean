15eea2ef8428d58dddc4b11ac952e7c0ad37f3ec
Domenic0Ferrari <domenico99ferrari@gmail.com>
2023-07-04 16:38:02 +0200

    add style in navbar

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,6 +1,5 @@
-<header class="bg-primary text-light p-2 mb-4 d-flex justify-content-center gap-3">
-    <a href="{{ route('home') }}" class="btn btn-light">HOME</a>
-    <a href="{{ route('about') }}" class="btn btn-light">ABOUT US</a>
-    <a href="{{ route('travels.index') }}" class="btn btn-light">YOUR TRAVELS</a>
-    <a href="{{ route('travels.trashed') }}" class="btn btn-light">BIN</a>
+<header class="text-light p-3 mb-4">
+    <div class="container">
+        @include('partials.navbar')
+    </div>
 </header>
